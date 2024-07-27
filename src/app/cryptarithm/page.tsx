6fbab637ace83e310221901b936f98e4196dd567
Mohamad Hanifan <mohamad.hanifan@efishery.com>
2024-07-27 13:10:03 +0700

    feat: handle input

--- src/app/cryptarithm/page.tsx
+++ src/app/cryptarithm/page.tsx
@@ -1,19 +1,31 @@
+'use client'
+
 import cx from 'classnames'
+import { useCallback, useState } from 'react'
 
 interface IDigitProps {
   char: string
   isActive?: boolean
   status?: 'valid' | 'invalid' | 'normal'
   value?: string
+  onSelect: () => void
 }
 
 interface IButtonInput {
   char: string
+  onSelect: () => void
 }
 
-function Digit({ char, status = 'normal', value, isActive }: IDigitProps) {
+function Digit({
+  char,
+  status = 'normal',
+  value,
+  isActive,
+  onSelect,
+}: IDigitProps) {
   return (
     <div
+      onClick={onSelect}
       className={cx('m-2 bg-slate-600 rounded-lg p-1', {
         'bg-yellow-600': status === 'invalid',
         'bg-green-700': status === 'valid',
@@ -21,9 +33,9 @@ function Digit({ char, status = 'normal', value, isActive }: IDigitProps) {
     >
       <div
         className={cx(
-          'w-14 h-14 rounded-lg border-gray-700 bg-slate-900 text-3xl font-bold flex justify-center items-center',
+          'w-14 h-14 rounded-lg bg-slate-900 text-3xl font-bold flex justify-center items-center',
           {
-            'bg-slate-700': isActive,
+            'border-2 border-yellow-200': isActive,
           }
         )}
       >
@@ -34,61 +46,173 @@ function Digit({ char, status = 'normal', value, isActive }: IDigitProps) {
   )
 }
 
-function ButtonInput({ char }: IButtonInput) {
+function ButtonInput({ char, onSelect }: IButtonInput) {
   return (
-    <button className='bg-gray-500 rounded-lg w-24 h-12 flex justify-center items-center mx-1 my-2 text-2xl hover:bg-gray-700 cursor-pointer font-semibold'>
+    <button
+      onClick={onSelect}
+      className='bg-gray-500 rounded-lg w-24 h-12 flex justify-center items-center mx-1 my-2 text-2xl hover:bg-gray-700 cursor-pointer font-semibold'
+    >
       {char}
     </button>
   )
 }
 
 export default function Home() {
+  const problemString = 'ADA_DI_DIA'
+
+  const [problem] = useState<string[][]>(
+    problemString.split('_').map((s) => s.split(''))
+  )
+
+  const [values, setValues] = useState<string[][]>(
+    problemString.split('_').map((s) => s.split('').map((c) => ''))
+  )
+
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  const isActive = useCallback(
+    (rowIndex: number, colIndex: number): boolean => {
+      const idx = rc2Idx(problem, rowIndex, colIndex)
+      return idx === activeIndex
+    },
+    [problem, activeIndex]
+  )
+
+  function handleSelectDigit(rowIndex: number, colIndex: number) {
+    const idx = rc2Idx(problem, rowIndex, colIndex)
+    setActiveIndex(idx)
+  }
+
+  function handleInput(val: string) {
+    const [r, c] = idx2RC(problem, activeIndex)
+    if (r < 0 || c < 0) return
+
+    // Handle Delete
+    if (val === 'X') {
+      // Delete Current Index
+      if (values[r][c] !== '') {
+        const newValues = [...values]
+        newValues[r][c] = ''
+        setValues(newValues)
+
+        // Delete previous index
+      } else {
+        const prevIdx = Math.max(0, activeIndex - 1)
+        const [r2, c2] = idx2RC(problem, prevIdx)
+        const newValues = [...values]
+        newValues[r2][c2] = ''
+        setValues(newValues)
+        setActiveIndex(prevIdx)
+      }
+    } else if (val === '') {
+    } else {
+      const newValues = [...values]
+      newValues[r][c] = val
+      setValues(newValues)
+      if (activeIndex < maxIdx(problem)) {
+        setActiveIndex((curr) => curr + 1)
+      }
+    }
+  }
+
   return (
-    <main className='flex min-h-screen w-full max-w-xl flex-col items-center justify-between m-auto text-white pb-12'>
+    <main className='flex min-h-screen w-full max-w-xl flex-col items-center justify-between m-auto text-white pb-8'>
       {/* Board */}
       <div className='pt-8'>
         <div className='flex flex-row justify-end mb-1 px-8'>
-          <Digit char='A' status='invalid' value='1' />
-          <Digit char='D' status='valid' value='2' isActive />
-          <Digit char='A' value='3' />
+          {problem[0] &&
+            problem[0].map((c, i) => (
+              <Digit
+                key={`${0}--${i}`}
+                char={c}
+                value={values[0][i]}
+                isActive={isActive(0, i)}
+                onSelect={() => handleSelectDigit(0, i)}
+              />
+            ))}
         </div>
         <div className='flex flex-row justify-end mb-1 px-8'>
-          <Digit char='D' />
-          <Digit char='I' />
+          {problem[1] &&
+            problem[1].map((c, i) => (
+              <Digit
+                key={`${1}--${i}`}
+                char={c}
+                value={values[1][i]}
+                isActive={isActive(1, i)}
+                onSelect={() => handleSelectDigit(1, i)}
+              />
+            ))}
         </div>
         <div className='flex flex-col items-end'>
           <p className='text-xl'>+</p>
           <hr className='min-w-full border-2 mb-3' />
         </div>
         <div className='flex flex-row justify-end mb-1 px-8'>
-          <Digit char='D' />
-          <Digit char='I' />
-          <Digit char='A' />
+          {problem[2] &&
+            problem[2].map((c, i) => (
+              <Digit
+                key={`${2}--${i}`}
+                char={c}
+                value={values[2][i]}
+                isActive={isActive(2, i)}
+                onSelect={() => handleSelectDigit(2, i)}
+              />
+            ))}
         </div>
       </div>
       {/* Keyboard */}
       <div className='pb-8'>
-        <div className='flex flex-row'>
-          <ButtonInput char='1' />
-          <ButtonInput char='2' />
-          <ButtonInput char='3' />
-        </div>
-        <div className='flex flex-row'>
-          <ButtonInput char='4' />
-          <ButtonInput char='5' />
-          <ButtonInput char='6' />
-        </div>
-        <div className='flex flex-row'>
-          <ButtonInput char='7' />
-          <ButtonInput char='8' />
-          <ButtonInput char='9' />
-        </div>
-        <div className='flex flex-row'>
-          <ButtonInput char='' />
-          <ButtonInput char='0' />
-          <ButtonInput char='x' />
-        </div>
+        {[
+          ['1', '2', '3'],
+          ['4', '5', '6'],
+          ['7', '8', '9'],
+          ['', '0', 'X'],
+        ].map((row, rowIdx) => (
+          <div key={rowIdx} className='flex flex-row'>
+            {row.map((c, colIdx) => (
+              <ButtonInput
+                key={`${rowIdx}--${colIdx}`}
+                char={c}
+                onSelect={() => handleInput(c)}
+              />
+            ))}
+          </div>
+        ))}
       </div>
     </main>
   )
+}
+
+function rc2Idx(problem: string[][], r: number, c: number): number {
+  let _idx = 0
+  for (let i = 0; i < problem.length; i++) {
+    for (let j = 0; j < problem[i].length; j++) {
+      if (r === i && c === j) {
+        return _idx
+      }
+      _idx++
+    }
+  }
+  return _idx
+}
+
+function idx2RC(problem: string[][], idx: number): number[] {
+  let _idx = 0
+  for (let i = 0; i < problem.length; i++) {
+    for (let j = 0; j < problem[i].length; j++) {
+      if (idx === _idx) {
+        return [i, j]
+      }
+      _idx++
+    }
+  }
+  return [-1, -1]
+}
+
+function maxIdx(problem: string[][]) {
+  let cnt = 0
+  for (let i = 0; i < problem.length; i++) {
+    cnt += problem[i].length
+  }
+  return cnt - 1
 }
