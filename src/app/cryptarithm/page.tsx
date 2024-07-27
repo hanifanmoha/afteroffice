5ced65f333ff96989d272facc545784f72ac75e6
Mohamad Hanifan <mohamad.hanifan@efishery.com>
2024-07-27 11:48:14 +0700

    feat: add cursor

--- src/app/cryptarithm/page.tsx
+++ src/app/cryptarithm/page.tsx
@@ -6,6 +6,10 @@ interface IDigitProps {
   value?: string
 }
 
+interface IButtonInput {
+  char: string
+}
+
 function Digit({ char, status = 'normal', value }: IDigitProps) {
   return (
     <div
@@ -26,11 +30,19 @@ function Digit({ char, status = 'normal', value }: IDigitProps) {
   )
 }
 
+function ButtonInput({ char }: IButtonInput) {
+  return (
+    <div className='bg-gray-500 rounded-lg w-24 h-12 flex justify-center items-center mx-1 my-2 text-2xl hover:bg-gray-700 cursor-pointer font-semibold'>
+      {char}
+    </div>
+  )
+}
+
 export default function Home() {
   return (
     <main className='flex min-h-screen w-full max-w-xl flex-col items-center justify-between m-auto text-white'>
       {/* Board */}
-      <div className='py-8'>
+      <div className='pt-8'>
         <div className='flex flex-row justify-end mb-4 px-8'>
           <Digit char='A' status='invalid' value='1' />
           <Digit char='D' status='valid' value='2' />
@@ -51,8 +63,27 @@ export default function Home() {
         </div>
       </div>
       {/* Keyboard */}
-      <div className=''>
-        <p>Hello World</p>
+      <div className='pb-8'>
+        <div className='flex flex-row'>
+          <ButtonInput char='1' />
+          <ButtonInput char='2' />
+          <ButtonInput char='3' />
+        </div>
+        <div className='flex flex-row'>
+          <ButtonInput char='4' />
+          <ButtonInput char='5' />
+          <ButtonInput char='6' />
+        </div>
+        <div className='flex flex-row'>
+          <ButtonInput char='7' />
+          <ButtonInput char='8' />
+          <ButtonInput char='9' />
+        </div>
+        <div className='flex flex-row'>
+          <ButtonInput char='' />
+          <ButtonInput char='0' />
+          <ButtonInput char='x' />
+        </div>
       </div>
     </main>
   )
